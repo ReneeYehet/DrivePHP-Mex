e4ea429668b4674debcd1aab92305748ce5750a5
Renee Espinoza <reneeespinoza3@gmail.com>
2020-10-23 08:06:27 -0700

    Intento de URL

--- DriveMex/php/index.php
+++ DriveMex/php/index.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
 		<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	</head>
@@ -19,7 +19,7 @@
 				const url = window.location.search;
 				const urlParams = new URLSearchParams(url);
 				const id_cot = urlParams.get('id');
-				var ruta = "php/insert-items.php";
+				var ruta = "insert-items.php";
 				const tipo = urlParams.get('tipo');
 
 			    $.ajax({type:'POST',        // call php 
@@ -31,10 +31,10 @@
 			    	success: function(insertar){
 			    		$('.bar').css('display', 'none');
 			    		if (insertar === "404") {
-				        	$('.content').load('plantillas/error-page.php');
+				        	$('.content').load('../plantillas/error-page.php');
 				        	$('.error_msg').append(insertar);
 			    		}else{
-				       		$('.content').load('plantillas/success-page.php');
+				       		$('.content').load('../plantillas/success-page.php');
 				       		$('.error_msg').append(insertar);
 			    		}
 				 	   
@@ -48,7 +48,7 @@
 		
 		<div class="error_msg" style="width: 100%; text-align: center;"> </div>
 		<div class="footer">
-			<img src="img/logo_ECN.png" style="width: 300;">
+			<img src="../img/logo_ECN.png" style="width: 300;">
 		</div>
 	</body>
 </html>
