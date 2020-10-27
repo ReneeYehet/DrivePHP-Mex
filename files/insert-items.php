1def5a808c3cb59afb18454f3920267429ae016e
Renee Espinoza <reneeespinoza3@gmail.com>
2020-10-27 10:03:20 -0700

    Prueba 22

--- files/insert-items.php
+++ files/insert-items.php
@@ -6,27 +6,29 @@ header("Access-Control-Allow-Methods: GET, POST");
 header("Allow: POST");
 
 
-	echo $id_cot = isset($_POST['id']) ? $_POST['id'] : 0;
-	echo $tipo= isset($_POST['tipo']) ? $_POST['tipo'] : 0;
+$id_cot = isset($_POST['id']) ? $_POST['id'] : 0;
+print $id_cot;
+$tipo= isset($_POST['tipo']) ? $_POST['tipo'] : 0;
+print $tipo;
 
-	switch ($tipo) {
-		case '1':
-			echo "entro";
-			/*$data = array();
-			$url = GetURL_Mex($id_cot);
-			$data = feed($url);
-			$insertar = InsertXML_Mex($data, $id_cot);
-			if ($insertar) {
-				echo "201";
-			}else{
-				echo "404";
-			}*/
-			break;
-		
-		default:
-			# code...
-			break;
-	}
+switch ($tipo) {
+	case 1:
+		echo "entro";
+		/*$data = array();
+		$url = GetURL_Mex($id_cot);
+		$data = feed($url);
+		$insertar = InsertXML_Mex($data, $id_cot);
+		if ($insertar) {
+			echo "201";
+		}else{
+			echo "404";
+		}*/
+		break;
+	
+	default:
+		# code...
+		break;
+}
 
 /*function isXmlHttpRequest()
 {
