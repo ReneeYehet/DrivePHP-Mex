04ba626612107bd3e3eda81c7b7fd86fd4ee035d
Renee Espinoza <reneeespinoza3@gmail.com>
2020-10-26 17:16:04 -0700

    Prueba 6

--- index.php
+++ index.php
@@ -4,47 +4,17 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/xml.js"></script>
 	</head>
 
 	<body>
 		<div class="content">
-			<!--<div class="bar">
+			<div class="bar">
 			  <div class="circle"></div>
 			  <p id="load">Agregando<br>datos</p>
-			</div>-->
+			</div>
 		</div>
-		
-		<script type="text/javascript">
-			$(document).ready(function(){
-				const url = window.location.search;
-				const urlParams = new URLSearchParams(url);
-				const id_cot = urlParams.get('id');
-				var ruta = "index.php/php/insert-items.php";
-				const tipo = urlParams.get('tipo');
-
-			    $.ajax({type:'POST',        // call php 
-			        url: ruta,
-			        data: ({
-			        	id :  id_cot, 
-			        	tipo : tipo
-			        }),
-			    	success: function(insertar){
-			    		$('.bar').css('display', 'none');
-			    		if (insertar === "404") {
-				        	$('.content').load('plantillas/error-page.php');
-				        	$('.error_msg').append(insertar);
-			    		}else{
-				       		$('.content').load('plantillas/success-page.php');
-				       		$('.error_msg').append(insertar);
-			    		}
-				 	   
-					},
-			    });
-			    console.log(id_cot);
-			    console.log(tipo);
-			});
-		</script>
-
+	
 		
 		<div class="error_msg" style="width: 100%; text-align: center;"> </div>
 		<div class="footer">
---
+++ js/xml.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+	const url = window.location.search;
+    const urlParams = new URLSearchParams(url);
+    const id_cot = urlParams.get('id');
+    const tipo = urlParams.get('tipo');
+
+    CallPHP(id_cot, tipo);
+});
+
+function CallPHP(id_cot, tipo){
+    
+    var ruta = "index.php/php/insert-items.php";
+
+    $.ajax({type:'POST',        // call php 
+        url: ruta,
+        data: ({
+            id :  id_cot, 
+            tipo : tipo
+        }),
+        success: function(insertar){
+            $('.bar').css('display', 'none');
+            if (insertar === "404") {
+                $('.content').load('plantillas/error-page.php');
+                $('.error_msg').append(insertar);
+            }else{
+                $('.content').load('plantillas/success-page.php');
+                $('.error_msg').append(insertar);
+            }
+            
+        },
+    });
+    console.log(id_cot);
+    console.log(tipo);
+}
