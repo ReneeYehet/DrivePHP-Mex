21dada128e5f30fc552b3b43557c16bbd90911d4
Renee Espinoza <reneeespinoza3@gmail.com>
2020-10-27 10:09:14 -0700

    Prueba 23

--- files/insert-items.php
+++ files/insert-items.php
@@ -11,24 +11,20 @@ print $id_cot;
 $tipo= isset($_POST['tipo']) ? $_POST['tipo'] : 0;
 print $tipo;
 
-switch ($tipo) {
-	case 1:
-		echo "entro";
-		/*$data = array();
-		$url = GetURL_Mex($id_cot);
-		$data = feed($url);
-		$insertar = InsertXML_Mex($data, $id_cot);
-		if ($insertar) {
-			echo "201";
-		}else{
-			echo "404";
-		}*/
-		break;
-	
-	default:
-		# code...
-		break;
-}
+XML($id_cot);
+
+function XML($id){
+	$data = array();
+	$url = GetURL_Mex($id_cot);
+	$data = feed($url);
+	$insertar = InsertXML_Mex($data, $id_cot);
+	if ($insertar) {
+		echo "201";
+	}else{
+		echo "404";
+	}
+} 
+
 
 /*function isXmlHttpRequest()
 {
