63df670cc1683f3fb60c42c6941012a829399df4
Renee Espinoza <reneeespinoza3@gmail.com>
2020-10-27 10:17:07 -0700

    Prueba 25

--- files/insert-items.php
+++ files/insert-items.php
@@ -1,4 +1,9 @@
 <?php
+include '../conexion-drive.php';
+
+include('DrivePHP.php');
+include('feed.php');
+
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json, text/plain');
@@ -11,19 +16,15 @@ print $id_cot;
 $tipo= isset($_POST['tipo']) ? $_POST['tipo'] : 0;
 print $tipo;
 
-XML($id_cot);
-
-function XML($id){
-	$data = array();
-	$url = GetURL_Mex($id_cot);
-	$data = feed($url);
-	$insertar = InsertXML_Mex($data, $id_cot);
-	if ($insertar) {
-		echo "201";
-	}else{
-		echo "404";
-	}
-} 
+$data = array();
+$url = GetURL_Mex($id_cot);
+$data = feed($url);
+$insertar = InsertXML_Mex($data, $id_cot);
+if ($insertar) {
+	echo "201";
+}else{
+	echo "404";
+}
 
 
 /*function isXmlHttpRequest()
@@ -43,158 +44,6 @@ else
 	
 	
 }*/
-
-/////////////////////////////////////////////////////////////// MEXICO
-function GetURL_Mex($id){
-	require_once '../conexion-drive.php';
-	// Get our spreadsheet
-$spreadsheet = (new Google\Spreadsheet\SpreadsheetService)
-   ->getSpreadsheetFeed()
-   ->getByTitle('Proceso Cotizaciones');
-
-// Get the first worksheet (tab)
-$worksheets = $spreadsheet->getWorksheetFeed()->getEntries();
-$worksheet = $worksheets[4];
-
-$listFeed = $worksheet->getListFeed();
-/** @var ListEntry */
-	foreach ($listFeed->getEntries() as $entry) {
-	   $representative = $entry->getValues();
-	   //echo json_encode($representative);
-	   if ($entry->getValues()['idcotizaciónbase'] === $id) {
-	   	$link = $entry->getValues()['xmlurl'];
-	   	
-	   }
-	}
-	//echo $link;
-	return $link;
-}
-
-///////////////////////////////////////
-
-function InsertXML_Mex($data, $id){
-	require_once '../conexion-drive.php';
-	$spreadsheet = (new Google\Spreadsheet\SpreadsheetService)
-	->getSpreadsheetFeed()
-	->getByTitle('Proceso Cotizaciones');
-
-	// Get the first worksheet (tab)
-	$worksheets = $spreadsheet->getWorksheetFeed()->getEntries();
-	$worksheet  = $worksheets[5];
-	
-	$listFeed = $worksheet->getListFeed();
-
-		for($i=0; $i < $data['lenght']; $i++) {
-			if (strpos($data['noitem'][$i], "-") !== false) {
-				$image = substr(strstr($data['noitem'][$i], '-', true), 0);
-			}else{
-				$image = $data['noitem'][$i];
-			}
-			
-			$listFeed->insert([
-			   'idcotizaciónpartidas' => $uniquecp = generateRandomString(),
-			   'idcotizaciónbase' => $id,
-			   'partnumber' => $data['noitem'][$i],
-			   'encabezado' => $data['title'][$i],
-			   'spk1' => 'E+H',
-			   'marca' => 'E+H',
-			   'descripción' => $descripcion = Description($data['desc'][$i]),
-			   'imagenurl' => 'https://portal.endress.com/wa002/picturepool/500/1/'.$image.'.jpg',
-			   'displayimagen' => '=IMAGE(RC[-1])',
-			   'tiempodeentrega' => '2 semanas',
-			   'qty' => $data['qty'][$i],
-			   'preciounitario' => $data['pu'][$i]
-			]);
-		
-		}
-	return json_encode($listFeed);
-}
-
-//////////////////////////////////////////////////////////// Funciones Random
-function generateRandomString() {
-	$length = 8;
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-
-function Description($des){
-	$space = str_replace("\r\n", '', $des);
-	$texto2 = preg_replace('/[0-9][0-9][0-9]+[:]/', "\r\n", $space);
-	$texto3 = preg_replace('/[A-Z]+[:]/', '', $texto2);
-	$texto4 = preg_replace('/[0-9]+[:]/', '', $texto3);
-
-	return $texto4;
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-///////////////// PARA AGREGAR XML A COTIZACIÓN PARTIDA PDF //////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-function feed($feedURL){
-
-	//obtener la url del file XML en la base de datos de appsheet
-	$xml = $feedURL;
-	$datos = array();
-
-	//Llamada a la url file xml para obtener su contenido
-	$curl = curl_init();
-	$header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,";
-	$header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
-	$header[]  = "Cache-Control: max-age=0";
-	$header[]  = "Connection: keep-alive";
-	$header[]  = "Keep-Alive: 300";
-	$header[]  = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
-	$header[]  = "Accept-Language: en-us,en;q=0.5";
-	$header[] = ""; // browsers keep this blank.
-
-	curl_setopt($curl, CURLOPT_URL, $xml);
-	curl_setopt($curl, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.google.com/bot.html)');
-	curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-	curl_setopt($curl, CURLOPT_REFERER, 'http://www.google.com');
-	curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate');
-	curl_setopt($curl, CURLOPT_AUTOREFERER, true);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-	
-	//se guarda en una variable para extraer los datos
-	$html = curl_exec($curl);
-	curl_close($curl);
-	//echo json_encode($html);
-
-	//comienza el proceso de extraccion de datos en el xml y se pasan a variables
-	$eh = simplexml_load_string($html);
-	
-	foreach ($eh->xpath("/bas:basket/bas:item") as $item) {
-		$noitem = $item->xpath("//bas:itemNo");
-		$iditem = $item->xpath("//bas:product/bas:orderCode");
-		$qty	= $item->xpath("//bas:product/bas:quantity");
-		$title	= $item->xpath("//bas:product/bas:texts/bas:longDescription");
-		$desc 	= $item->xpath("//bas:product/bas:texts/bas:configuration");
-		$cu		= implode(" ", $item->xpath("bas:itemPricing/bas:itemListPrice/@currency"));
-		$pl		= $item->xpath("//bas:itemPricing/bas:itemListPrice");
-		$des	= $item->xpath("//bas:itemPricing/bas:itemDiscount");
-		$pu 	= $item->xpath("//bas:itemPricing/bas:unitSalesPrice");
-		$total 	= $item->xpath("//bas:itemPricing/bas:itemSalesPrice");
-
-		
-	}
-
-		$long = count($eh->xpath("/bas:basket/bas:item"));
-
-		$datos = array('item' => $noitem, 'noitem' => $iditem, 'qty' => $qty, 'title' => $title, 'desc' => $desc, 'cu' => $cu, 'des' => $des, 'pl' => $pl, 'pu' => $pu, 'total' => $total, 'lenght' => $long);
-	
-		//echo json_encode($datos);
-
-		return $datos;
-}
-
-
 
 
 /*switch ($tipo) {
