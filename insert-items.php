8bc72f362a0805b48855f53ae11e5d244b3c8525
Renee Espinoza <reneeespinoza3@gmail.com>
2020-10-28 08:51:20 -0700

    Prueba 36

--- insert-items.php
+++ insert-items.php
@@ -3,26 +3,6 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json, text/plain');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
 
-require __DIR__ . '/vendor/autoload.php';
-use Google\Spreadsheet\DefaultServiceRequest;
-use Google\Spreadsheet\ServiceRequestFactory;
-
-putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/client_secret_mex.json');
-$client = new Google_Client;
-$client->useApplicationDefaultCredentials();
-
-$client->setApplicationName("Drive PHP Mex");
-$client->setScopes(Google_Service_Drive::DRIVE);
-
-if ($client->isAccessTokenExpired()) {
-    $client->refreshTokenWithAssertion();
-}
-
-$accessToken = $client->fetchAccessTokenWithAssertion()["access_token"];
-
-ServiceRequestFactory::setInstance(
-    new DefaultServiceRequest($accessToken)
-);
 
 XML();
 print "Hello";
@@ -37,6 +17,7 @@ function XML(){
 
 /////////////////////////////////////////////////////////////// MEXICO
 function GetURL_Mex($id){
+	require_once 'conexion-drive.php';
 	echo "Hello";
 }
 
